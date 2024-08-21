78babb3ad4a98dab44d07d1ea19a70d12b22e876
krishna-dubeey <97288669+krishna-dubeey@users.noreply.github.com>
2024-08-21 22:41:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,3 +122,5 @@ if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render will set PORT; default to 5000 locally
+    app.run(host="0.0.0.0", port=port)
